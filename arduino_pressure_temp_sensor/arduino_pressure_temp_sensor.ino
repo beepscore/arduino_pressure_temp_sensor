6956b9806c07430f0c170a961d5c9a3e5cbd9bd7
beepscore <support@beepscore.com>
2013-08-10 14:57:17 -0700

    Re-indent.

--- arduino_pressure_temp_sensor/arduino_pressure_temp_sensor.ino
+++ arduino_pressure_temp_sensor/arduino_pressure_temp_sensor.ino
@@ -7,8 +7,7 @@
   Designed specifically to work with the Adafruit BMP085 Breakout 
   ----> https://www.adafruit.com/products/391
 
-  These displays use I2C to communicate, 2 pins are required to  
-  interface
+  These displays use I2C to communicate, 2 pins are required to interface
   Adafruit invests time and resources providing this open source code, 
   please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
@@ -25,38 +24,38 @@
 // XCLR is a reset pin, also not used here
 
 Adafruit_BMP085 bmp;
-  
+
 void setup() {
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-  }
+    Serial.begin(9600);
+    if (!bmp.begin()) {
+        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+        while (1) {}
+    }
 }
-  
+
 void loop() {
     Serial.print("Temperature = ");
     Serial.print(bmp.readTemperature());
     Serial.println(" *C");
-    
+
     Serial.print("Pressure = ");
     Serial.print(bmp.readPressure());
     Serial.println(" Pa");
-    
+
     // Calculate altitude assuming 'standard' barometric
     // pressure of 1013.25 millibar = 101325 Pascal
     Serial.print("Altitude = ");
     Serial.print(bmp.readAltitude());
     Serial.println(" meters");
 
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
+    // you can get a more precise measurement of altitude
+    // if you know the current sea level pressure which will
+    // vary with weather and such. If it is 1015 millibars
+    // that is equal to 101500 Pascals.
     Serial.print("Real altitude = ");
     Serial.print(bmp.readAltitude(101500));
     Serial.println(" meters");
-    
+
     Serial.println();
     delay(500);
 }
